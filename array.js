1d0fab01d9c4fd9c8af9b39e02290c351fb5165e
madhukesh <madhukeshpoddar@github.com>
2016-05-05 18:16:16 +0530

    my assign

--- array.js
+++ array.js
@@ -1,3 +1,13 @@
 var grades = [50,70];
 grades.push(20);
 console.log(grades);
+//
+var grades = [100,200,300];
+console.log	(grades[0]);
+grades.push (97);
+grades.forEach(function(grade){
+
+});
+
+console.log(grades.length);
+
---
+++ for.js
@@ -0,0 +1,17 @@
+var  i = 0;
+var count = 8;
+while(i<count)
+{
+	console.log ('while:' + i);
+	 i++;
+
+	
+	}
+//
+	
+	var i=0;
+	var count =8;
+	 for(i=0;i<count;i++)
+	 {
+	 	console.log('for:' + i)
+	 }
