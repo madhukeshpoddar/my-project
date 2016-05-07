81d5651c023edca6842fd03bd74ca838a23f0b47
madhukesh <madhukeshpoddar@github.com>
2016-05-07 14:13:50 +0530

    fljskf

---
+++ args.js
@@ -0,0 +1,27 @@
+var argv = require('yargs')
+ .command('hello' , 'Greets the user', function(yargs){
+ 	yeags.option({
+
+ 	});
+ })
+
+  var command =argv._[0]
+
+  console.log(argv)
+  if(command== 'hello' && typeof argv.name !== 'undefind' && typeof argv.lastname)
+}
+console.log('hello'+argv.name+'' +argv.lastname+ '!');
+console.log('Hello' +argv.name+ !);
+}
+else if(command === 'hello' && typeof arrgv.name !== 'undefind' && typeof argv.lastname)
+}
+console.log('hello' + arv.name!= 'underfined'){
+	else if(command === 'hello')
+	{
+		console.log('hello world'){
+			else if(command=== 'hello'){
+				console.log('hello');
+			}
+		}
+	}
+}
